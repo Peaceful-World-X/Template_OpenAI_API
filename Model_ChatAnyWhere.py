67d0f46327787ae61d217bd60ab90bee7c05fbe5
耗不尽的先生 <Peaceful_World@qq.com>
2025-07-10 21:54:39 +0800

    Update Model_ChatAnyWhere.py

--- Model_ChatAnyWhere.py
+++ Model_ChatAnyWhere.py
@@ -9,25 +9,37 @@ headers = {
 }
 response = requests.get(url, headers=headers)
 models = response.json().get("data", [])
-for model in models:
-    print(model.get("id"))
+
+# 使用集合去重并排序
+unique_sorted_models = sorted(set(model.get("id") for model in models))
+
+# 打印排序后的模型ID
+for model_id in unique_sorted_models:
+    print(model_id)
 
 '''
-gpt-4o-mini-2024-07-18
-gpt-4o-mini
-text-embedding-3-large
+deepseek-chat
+deepseek-r1
+deepseek-r1-250528
+deepseek-v3
 gpt-3.5-turbo
-gpt-3.5-turbo
-gpt-4o-ca
-gpt-4o-mini-ca
+gpt-3.5-turbo-0125
+gpt-3.5-turbo-1106
+gpt-3.5-turbo-ca
+gpt-4.1
+gpt-4.1-mini
+gpt-4.1-mini-2025-04-14
+gpt-4.1-nano
+gpt-4.1-nano-2025-04-14
 gpt-4o
 gpt-4o-2024-05-13
-deepseek-r1
-gpt-3.5-turbo-ca
-text-embedding-3-small
-deepseek-v3
+gpt-4o-ca
+gpt-4o-mini
+gpt-4o-mini-2024-07-18
 gpt-4o-mini-2024-07-18-ca
+gpt-4o-mini-ca
+text-embedding-3-large
+text-embedding-3-small
 text-embedding-ada-002
-gpt-3.5-turbo-0125
 '''
 
