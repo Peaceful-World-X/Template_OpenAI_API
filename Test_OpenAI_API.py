3cb23218da5b195abbf35ccca2d38075bacd1d91
耗不尽的先生 <Peaceful_World@qq.com>
2025-07-14 11:49:23 +0800

    Update Test_OpenAI_API.py

--- Test_OpenAI_API.py
+++ Test_OpenAI_API.py
@@ -1,16 +1,39 @@
 from openai import OpenAI
 
-client = OpenAI(
-    api_key="AIzaSyC3Rx_g_pQKZNmiYEYldN170JHJ2dVTmhA",
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-)
+# stream=True
+stream=False
 
-response = client.chat.completions.create(
-    model="gemini-2.5-flash-preview-tts",
-    messages=[
-        {"role": "system","content": "回答内容请保持在100个字以内。"},
-        {"role": "user","content": "你是什么大模型?回答请包括模型名、你的开发者、该模型具体版本号、该模型最后的训练时间。"}
-    ]
-)
+KEY = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
+model = "MiniMax/MiniMax-M1-80k"
+sys_message  = "回答内容请保持在100个字以内。"
+user_message = "你是什么大模型?回答请包括模型名、你的开发者、该模型具体版本号、该模型最后的训练时间。"
+client = OpenAI(api_key=KEY, base_url=URL)
 
-print(response.choices[0].message.content)
+# 流式输出
+if stream:
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system","content": sys_message},
+            {"role": "user","content": user_message}
+        ],
+        stream=True
+    )
+    for chunk in response:
+        if not hasattr(chunk, "choices") or not chunk.choices:
+            continue
+        message = chunk.choices[0].delta
+        if hasattr(message, "content") and message.content:
+            print(message.content)
+
+# 非流式输出
+else:
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system","content": sys_message},
+            {"role": "user","content": user_message}
+        ]
+    )
+    print(response.choices[0].message.content)
